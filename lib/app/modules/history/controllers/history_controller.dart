1006a605a3714436b2cc0bb7bd553cc89ae0dec8
serius <seriusman0@gmail.com>
2022-09-11 06:37:55 +0700

    history

--- lib/app/modules/history/controllers/history_controller.dart
+++ lib/app/modules/history/controllers/history_controller.dart
@@ -1,12 +1,76 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'dart:developer' as developer;
+import 'package:http/http.dart' as http;
+
+import '../../../../globals.dart';
 
 class HistoryController extends GetxController {
-  //TODO: Implement HistoryController
+  RxBool isLoading = true.obs;
+  var id_payment = [''].obs;
+  var pembeli = [''].obs;
+  var total_bayar = [0].obs;
+  var update_at = [''].obs;
+  var total_tagihan = [0].obs;
+  final server = Global.server;
+
+  Future<RxList<String>> getHistory() async {
+    try {
+      final response = await http.get(Uri.parse("$server/get_history.php"));
+      if (response.statusCode == 200) {
+        this.id_payment.removeRange(0, this.id_payment.length);
+        this.pembeli.removeRange(0, this.pembeli.length);
+        this.total_bayar.removeRange(0, this.total_bayar.length);
+        this.update_at.removeRange(0, this.update_at.length);
+        Get.snackbar("Oke", "Okeee");
+
+        for (int i = 0; i < (json.decode(response.body).length); i++) {
+          this.id_payment.add(jsonDecode(response.body)[i]['id_payment']);
+          this.pembeli.add(jsonDecode(response.body)[i]['pembeli']);
+          this
+              .total_bayar
+              .add(int.parse(jsonDecode(response.body)[i]['total_bayar']));
+          this.update_at.add(jsonDecode(response.body)[i]['update_at']);
+          this.pembeli.add(jsonDecode(response.body)[i]['pembeli']);
+        }
+      }
+    } catch (e) {
+      print("Error $e");
+    }
+    return this.id_payment;
+  }
+
+  Future<RxList<String>> getTotalTagihan() async {
+    try {
+      final response = await http.get(Uri.parse("$server/get_total_bayar.php"));
+      if (response.statusCode == 200) {
+        this.total_tagihan.removeRange(0, this.total_tagihan.length);
+        Get.snackbar("Oke", "Okeee");
+
+        for (int i = 0; i < total_bayar.length; i++) {
+          this
+              .total_bayar
+              .add(int.parse(jsonDecode(response.body)[i]['total_bayar']));
+        }
+      }
+    } catch (e) {
+      print("Error $e");
+    }
+    return this.id_payment;
+  }
 
   final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await getHistory();
+      await getTotalTagihan();
+      this.isLoading.value = await false;
+      developer.log("Loading data selesai");
+    });
   }
 
   @override
--- lib/app/modules/history/views/history_view.dart
+++ lib/app/modules/history/views/history_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
+import '../../../routes/app_pages.dart';
 import '../controllers/history_controller.dart';
 
 class HistoryView extends GetView<HistoryController> {
@@ -12,11 +14,90 @@ class HistoryView extends GetView<HistoryController> {
         title: Text('HistoryView'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'History Transaksi',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Column(
+        children: [
+          Expanded(
+              child: Obx(
+            () => (controller.isLoading.isTrue)
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    itemCount: controller.id_payment.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Card(
+                          child: Row(
+                            children: [
+                              Expanded(
+                                flex: 3,
+                                child: Container(
+                                  padding: EdgeInsets.only(left: 8),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text("${controller.pembeli[index]}"),
+                                      Text(
+                                          style: TextStyle(fontSize: 15),
+                                          NumberFormat.currency(
+                                                  locale: 'id',
+                                                  decimalDigits: 0,
+                                                  symbol: "Total bayar : Rp ")
+                                              .format(controller
+                                                  .total_bayar[index])),
+                                      Text(
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 20),
+                                          textAlign: TextAlign.right,
+                                          NumberFormat.currency(
+                                                  locale: 'id',
+                                                  decimalDigits: 0,
+                                                  symbol: "Total Bayar : Rp ")
+                                              .format(
+                                                  controller.total_tagihan[0])),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                  flex: 1,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      ElevatedButton(
+                                          onPressed: () {},
+                                          child: Icon(
+                                            Icons.edit,
+                                            color: Colors.green,
+                                          )),
+                                      ElevatedButton(
+                                          onPressed: () {
+                                            Get.defaultDialog(
+                                              textCancel: "Batal",
+                                              title: "Hapus Akun",
+                                              middleText:
+                                                  "Anda yakin akan tindakan ini? ${controller.id_payment[index]}",
+                                              textConfirm: "Ya",
+                                              confirmTextColor: Colors.white,
+                                              onCancel: () => Get.back(),
+                                              onConfirm: () {},
+                                            );
+                                          },
+                                          child: Icon(Icons.delete,
+                                              color: Colors.red)),
+                                    ],
+                                  ))
+                            ],
+                          ),
+                        ),
+                      );
+                    }),
+          )),
+        ],
       ),
     );
   }
